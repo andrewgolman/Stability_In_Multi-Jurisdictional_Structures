7766bb6749f0b5fa00b58bad17b5a64ad5a6263c
Andrew Golman <andrewsgolman@gmail.com>
2018-05-13 00:55:35 +0300

    Add new results

--- DoubleAllocation.py
+++ DoubleAllocation.py
@@ -1,4 +1,12 @@
 from typing import List
+from numpy import linspace
+
+
+class Stability():
+    def __init__(self, value, data):
+        self.value = round(value, 5)
+        self.data = data
+
 
 class Partition:
     def __init__(self, allocation, groups):
@@ -17,14 +25,14 @@ class Partition:
 
 
 class Group:
-    def __init__(self, allocation, left, right, med=-1):
+    def __init__(self, allocation, left, right, med=None):
         self.left_ = left
         self.right_ = right
         if left < right:
             self.median_ = allocation.dist
         elif left > right:
             self.median_ = 0
-        elif med == -1:
+        elif med is None:
             self.median_ = allocation.dist / 2
         else:
             self.median_ = med
@@ -34,7 +42,7 @@ class Group:
 
 
 class DoubleAllocation:
-    def __init__(self, left: int, right: int, dist: float):
+    def __init__(self, left: int, right: int, dist: float, enable_absolute_costs=False):
         if left > right:
             left, right = right, left
         self.dist = dist
@@ -42,6 +50,7 @@ class DoubleAllocation:
         self.right = right
         self.left_agents = [0] * self.left
         self.right_agents = [dist] * self.right
+        self.absolute_costs = enable_absolute_costs
 
     def to_the_segment(self, point: float) -> float:
         if point > self.dist:
@@ -54,12 +63,11 @@ class DoubleAllocation:
         return 1 / size + distance
 
     def delta(self, old, new):
+        if self.absolute_costs:
+            return old - new
         return (old-new)/old
 
     def stability_for_partition(self, partition: Partition):
-        # print(partition.left_costs_)
-        # print(partition.left_costs_.sort())
-        # print(list(reversed(sorted(partition.left_costs()))))
         left_costs = list(reversed(sorted(partition.left_costs())))
         right_costs = list(reversed(sorted(partition.right_costs())))
         ans = 0
@@ -71,21 +79,17 @@ class DoubleAllocation:
             ans = max(ans, delta_costs_right)
         for i in range(self.left):
             med = self.to_the_segment((left_costs[i] - right_costs[i] + self.dist) / 2)
-            # delta_costs_left = left_costs[i] - 1 / 2 / (i+1) - med
-            # delta_costs_right = right_costs[i] - 1 / 2 / (i+1) - self.dist + med
             delta_costs_left = self.delta(left_costs[i], self.costs(2*(i+1), med))
             delta_costs_right = self.delta(right_costs[i], self.costs(2*(i+1), self.dist - med))
             ans = max(ans, min(delta_costs_left, delta_costs_right))
 
-        for i in range(1, self.left): # TODO to be optimized
+        for i in range(1, self.left):
             for j in range(i):
-                # delta_costs_left = left_costs[i] - 1 / (i+j+2)
-                # delta_costs_right = right_costs[j] - 1 / (i+j+2) - self.dist
                 delta_costs_left = self.delta(left_costs[i], self.costs(i+j+2, 0))
                 delta_costs_right = self.delta(right_costs[i], self.costs(i+j+2, self.dist))
                 ans = max(ans, min(delta_costs_left, delta_costs_right))
 
-        for j in range(1, self.right):  # TODO to be optimized
+        for j in range(1, self.right):
             for i in range(min(j, self.left)):
                 delta_costs_left = self.delta(left_costs[i], self.costs(i + j + 2, self.dist))
                 delta_costs_right = self.delta(right_costs[i], self.costs(i + j + 2, 0))
@@ -93,18 +97,83 @@ class DoubleAllocation:
 
         return ans
 
+    def federation_groups(self, left, right):
+        return [Group(self, left, 0), Group(self, 0, right)]
+
+    def unity_groups(self, left, right):
+        return [Group(self, left, right)]
+
+    def undefmax_groups(self, left, right, median=None):
+        return [Group(self, left, left, median), Group(self, 0, right - left)]
+
     def federation(self):
-        return Partition(self, [Group(self, self.left, 0), Group(self, 0, self.right)])
+        return Partition(self, self.federation_groups(self.left, self.right))
 
     def unity(self):
-        return Partition(self, [Group(self, self.left, self.right)])
+        return Partition(self, self.unity_groups(self.left, self.right))
 
-    def dualmax(self):
-        return Partition(self, [Group(self, self.left, self.left), Group(self, 0, self.right - self.left)])
+    def undefmax(self):
+        return Partition(self, self.undefmax_groups(self.left, self.right))
 
-    def is_trivially_stable(self):
+    def one_middle_group_partitions(self):
+        ans = []
+        for i in range(1, self.left):
+            r = self.right - i
+            l = self.left - i
+            ans.append(Partition(self, self.federation_groups(l, r) + [Group(self, i, i)]))
+            ans.append(Partition(self, self.unity_groups(l, r) + [Group(self, i, i)]))
+            # ans.append(Partition(self, self.undefmax_groups(l, r) + [Group(self, i, i)]))
+        ans.append(self.federation())
+        ans.append(self.unity())
+        ans.append(self.undefmax())
+        return ans
+
+    def trivial_stability(self):
         a = self.stability_for_partition(self.federation())
         b = self.stability_for_partition(self.unity())
-        c = self.stability_for_partition(self.dualmax())
-        return min(a, b, c)
+        c = self.stability_for_partition(self.undefmax())
+        stability = min(a, b, c)
+        data = (int(1000*a), int(1000*b), int(1000*c))
+        return Stability(stability, data)
 
+    def stability_for_partitions(self):
+        partitions = self.one_middle_group_partitions()
+        ans = (1, None)
+        stat = []
+        for p in partitions:
+            cur = self.stability_for_partition(p)
+            stat.append(cur)
+            if cur < ans[0]:
+                ans = (cur, p)
+        return Stability(ans[0], ans[1])
+
+    def true_stability(self, probes):
+        # partition(self, allocation, groups):
+        # group(self, allocation, left, right, med=None):
+        ans = 1
+        for a in range(self.left):
+            print(a, ans)
+            for m in linspace(0, self.dist, probes):
+                for l in range(self.left - a):
+                    groups = list()
+                    if a:
+                        groups.append(Group(self, a, a, m))
+                    groups.append(Group(self, l, self.right))
+                    groups.append(Group(self, self.left - l, 0))
+                    partition = Partition(self, groups)
+                    next_val = self.stability_for_partition(partition)
+                    if next_val < ans:
+                        ans = next_val
+                        # print("l", a, m, l, ans)
+                for r in range(self.right - a):
+                    groups = list()
+                    if a:
+                        groups.append(Group(self, a, a, m))
+                    groups.append(Group(self, self.left, r))
+                    groups.append(Group(self, 0, self.right - r))
+                    partition = Partition(self, groups)
+                    next_val = self.stability_for_partition(partition)
+                    if next_val < ans:
+                        ans = next_val
+                        # print("r", a, m, r, ans)
+        return ans
--- tests.py
+++ tests.py
@@ -1,21 +1,59 @@
 from DoubleAllocation import *
 from numpy import linspace
-LIM = 0.0002
+from utils import *
+LIM = 0.01
+
+def test_non_trivial():
+    pairs = get_pairs()
+    for i, j in pairs[:10]:
+        res = (Stability(1, None), 0)
+        for d in linspace(0, 0.4, 21):
+            p = DoubleAllocation(i, j, d)
+            st = p.stability_for_partitions()
+            if st.value < res[0].value:
+                res = (st, d)
+        if res[0].value > LIM:
+            # print(i, j, res[1], res[0].value, res[0].data, file=file)
+            print(i, j, res[1], res[0].value, res[0].data)
 
 
-def test_trivial_all_to_n(n):
-    file = open("2coalitionsDIV.txt", "w")
-    for i in range(1, n):
-        for j in range(i+1, n):
-            res = (0, 0)
-            for d in linspace(0, 1, 501):
+def analyze():
+    pairs = get_pairs()
+    for i, j in pairs:
+        res = (Stability(0, None), 0)
+        for d in linspace(0, 0.1, 101):
+            p = DoubleAllocation(i, j, d)
+            st = p.trivial_stability()
+            if st.value > res[0].value:
+                res = (st, d)
+        if res[0].value > LIM:
+            # print(i, j, res[1], res[0].value, res[0].data, file=file)
+            print(i, j, res[1], res[0].value, res[0].data)
+
+
+def test_trivial_all_to_n(n, start=1, start_all=1):
+    # file = open("2coalitionsDIV.txt", "w")
+    for i in range(start_all, n):
+        if not i % 10:
+            print(i)
+        for j in range(max(start, i+1), min(n, int(i*1.63)+1)):
+            res = (Stability(0, None), 0)
+            for d in linspace(0, 1, 1000):
                 p = DoubleAllocation(i, j, d)
-                st = p.is_trivially_stable()
-                if st > res[0]:
+                st = p.trivial_stability()
+                if st.value > res[0].value:
                     res = (st, d)
-            if res[0] > LIM:
-                print(i, j, res[1], res[0], file=file)
-                print(i, j, res[1], res[0])
+            if res[0].value > LIM:
+                # print(i, j, res[1], res[0].value, res[0].data, file=file)
+                print(i, j, round(res[1], 4), res[0].value)
+
+
+def test_trivial_one_case(i, j, d):
+    p = DoubleAllocation(i, j, d)
+    st = p.trivial_stability()
+    res = (st, d)
+    print(res[0].value)
+    print(i, j, round(res[1], 8), res[0].value)
 
 
 def test_trivial_agents(n, m, create_file=False):
@@ -24,7 +62,7 @@ def test_trivial_agents(n, m, create_file=False):
     maxst = 0
     for d in linspace(0, 1, 2001):
         p = DoubleAllocation(n, m, d)
-        st = p.is_trivially_stable()
+        st = p.trivial_stability()
         if create_file and st >= LIM:
             print(d, st, file=file)
         maxst = max(st, maxst)
@@ -32,11 +70,71 @@ def test_trivial_agents(n, m, create_file=False):
 
 
 def simple_test():
-    print(DoubleAllocation(2, 3, 14 / 45).is_trivially_stable())
+    print(DoubleAllocation(2, 3, 14 / 45).trivial_stability().value)
     # assert(abs(DoubleAllocation(2, 3, 14 / 45).is_trivially_stable() - 1/90) < 0.0001)
 
 
+def test_useless_lowerbound_theorem():
+    data = get_data()
+    n = 0
+    for i in data:
+        l, r, d, eps = tuple(i)
+        if l > (4 / (1-eps) - 2) / d:
+            print(i, (4 / (1 - eps) - 2) / d)
+            n += 1
+    if not n:
+        print("useless")
+
+
+
+def lowerbound_costs_city(n, eps):
+    return 1 / n / (1 - eps)
+
+
+def lowerbound_costs_undefined(n, eps, d):
+    return (1 / 2 / n + d) / (1 - eps)
+
+
+def test_lowerbound_costs():
+    l, r, d, eps = 3, 4, 0.212, 0.057
+    for i in range(1, r+1):
+        print(round(1 / lowerbound_costs_city(i, eps)))
+    print()
+    for i in range(1, l+1):
+        print(lowerbound_costs_undefined(i, eps, d/2))
+
+
+def test_new_induction():
+    data = get_data()
+    for i in data:
+        l, r, d, eps = tuple(i)
+        if 1/2.95 * (1 + 2 * l * d) >= 1 - eps:
+            print(l, r, d, eps)
+            print(1 / 2 - eps, l * d)
+            print(int(l / ((1 / 4 - eps / 2) / d)))
+        # else:
+        #     print(l, r, d, eps)
+        #     print((1 / 4 - eps / 2) / d, l / 2)
+
+
+def test_true_stability():
+    l = 29
+    r = 38
+    d = 0.022
+    p = DoubleAllocation(l, r, d)
+    print(p.true_stability(200))
+
+
 def main():
-    simple_test()
+    # test_trivial_all_to_n(100, start=51, start_all=35)
+    # test_new_induction()
+    # test_trivial_one_case(1000, 1307, 0.64399554113739/1000)
+    # test_true_stability()
+    test_trivial_all_to_n(100)
+    # simple_test()
 
 main()
+
+# (0.06865687401542786, 1.306868686868687, 0.64399554113739832)
+
+# 0.05507279693486589
