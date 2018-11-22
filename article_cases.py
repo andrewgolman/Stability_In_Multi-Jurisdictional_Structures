c215d1d05e006027044e076965f39ae98fafbada
andrewsg <andrewsgolman@gmail.com>
2018-11-22 19:37:09 +0300

    adapt cases 3-5 for optimization

--- article_cases.py
+++ article_cases.py
@@ -1,7 +1,6 @@
 from DoubleAllocation import *
 from numpy import linspace
-from optimization import f
-# from optimization2 import f
+from upperbound071theorem import upperbound071_function
 
 
 def case_1():
@@ -18,45 +17,37 @@ def case_1():
                 print("L={}, R={}, d={}, eps <= {}".format(i, j, round(res[1], 4), res[0].value + 0.001))
 
 
-def case_2():
-    # l = 29
-    # r = 38
-    # d = 0.022
+def case_2(probes=2000):
     l = 56
     r = 73
     d = 0.6363245 / 56
     p = DoubleAllocation(l, r, d)
-    # print(p.trivial_stability())
-    print(p.true_stability(2000, verbose=True))
+    print(p.true_stability(probes=probes, verbose=True))
 
 
-def case_3():
-    # file = open("optim.txt", "a")
+def case_3(k_space, d_probes=101):
     data = 0, 0, 0
-    for k in linspace(1, 1.62, 101):
-        print(k)
-        for d in linspace(k / (1 + k), 1 / k, 101):
-            step = 12
-            next = f((k, d))
-            # if next > 0:
-            #     print(next, k, d, step, file=file)
+    for k in k_space:
+        print(f"done with k={k}")
+        for d in linspace(k / (1 + k), 1 / k, d_probes):
+            next = upperbound071_function((k, d))
             if next > data[0]:
                 data = next, k, d
-    print(data)
+    print("result:", data)
 
 
-def case_4():
-    for d in linspace(0.208, 0.209, 51):
+def case_4(space, probes=1000):
+    for d in space:
         p = DoubleAllocation(3, 4, d, enable_absolute_costs=True)
-        val = p.true_stability(1000)
+        val = p.true_stability(probes=probes)
         if val > 0.001:
             print(d, val)
 
 
-def case_5():
-    for d in linspace(0.1749, 0.1751, 11):
+def case_5(space, probes=1000):
+    for d in space:
         p = DoubleAllocation(4, 5, d, enable_absolute_costs=True)
-        val = p.true_stability(1000)
+        val = p.true_stability(probes=probes)
         if val > 0.001:
             print(d, val)
 
@@ -64,5 +55,6 @@ def case_5():
 # be careful, this may take up to...
 # case_1()  # a few hours, change LIM to see the progress or see the final results in the CASE_ABS.TXT
 case_2()  # 15 minutes
-# case_3()  # 15 minutes
-# case_5()
+# case_3(linspace(1, 1.62, 101), 101)  # 15 minutes
+# case_4(linspace(0.208, 0.209, 51))  # a few seconds
+# case_5(linspace(0.1749, 0.1751, 11))  # a few seconds
