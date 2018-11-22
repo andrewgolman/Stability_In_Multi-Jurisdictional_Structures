5af06123da9a50c3eda02480bd5ab4f2e435f96d
andrewsg <andrewsgolman@gmail.com>
2018-11-22 19:33:51 +0300

    unify functions for rel and abs cases

--- pixel_diagram.py
+++ pixel_diagram.py
@@ -1,49 +1,43 @@
-from numpy import linspace
+import numpy as np
 import scipy.misc as smp
 from two_group_upperbound import world_instability
 
 
-def diagram():
-    size = 4
+def draw_diagram(a_space, b_space, instability, size=64):
     data = np.zeros((size, size, 3), dtype=np.uint8)
-    for k, kp in zip(linspace(1, 2, size), range(size)):
-        for d, dp in zip(linspace(0.5, 1, size), range(size)):
-            instab, a = world_instability(k, d)
+    for a, a_pixel in zip(a_space, range(size)):
+        for b, b_pixel in zip(b_space, range(size)):
+            instab, alpha = instability(a, b)
             if instab < 0.0000001:
-                a = (0, "S")
-            if a[1] == "S":
-                data[kp, dp] = [255, 255, 255]
-            elif a[1] == "U":
-                data[kp, dp] = [255, 0, 0]
-            elif a[1] == "M":
-                data[kp, dp] = [0, 255, 0]
-            elif a[1] == "F":
-                data[kp, dp] = [0, 0, 255]
+                alpha = (0, "S")
+            if alpha[1] == "S":
+                data[a_pixel, b_pixel] = [255, 255, 255]
+            elif alpha[1] == "U":
+                data[a_pixel, b_pixel] = [255, 0, 0]
+            elif alpha[1] == "M":
+                data[a_pixel, b_pixel] = [0, 255, 0]
+            elif alpha[1] == "F":
+                data[a_pixel, b_pixel] = [0, 0, 255]
             else:
-                data[kp, dp] = [128 + int(a[1]) * 10]
+                data[a_pixel, b_pixel] = [128 + int(alpha[1]) * 10]
 
     img = smp.toimage(data)
     img.show()
 
 
-def diagram2():
-    size = 64
-    data = np.zeros((size, size, 3), dtype=np.uint8)
-    for k, kp in zip(linspace(1, 2, size), range(size)):
-        for d, dp in zip(linspace(1.6, 3.2, size), range(size)):
-            instab, a = world_instability(d/k, 1/k)
-            if instab < 0.0000001:
-                a = (0, "S")
-            if a[1] == "S":
-                data[kp, dp] = [255, 255, 255]
-            elif a[1] == "U":
-                data[kp, dp] = [255, 0, 0]
-            elif a[1] == "M":
-                data[kp, dp] = [0, 255, 0]
-            elif a[1] == "F":
-                data[kp, dp] = [0, 0, 255]
-            else:
-                data[kp, dp] = [128 + int(a[1]) * 10]
+def k_d_diagram(size):
+    k_space = np.linspace(1, 2, size)
+    d_space = np.linspace(0.5, 1, size)
+    instability = lambda k, d: world_instability(k, d)
+    draw_diagram(k_space, d_space, instability, size)
 
-    img = smp.toimage(data)
-    img.show()
+
+def relation_diagram(size):
+    a_space = np.linspace(1, 2, size)
+    b_space = np.linspace(1,6, 3.2, size)
+    instability = lambda a, b: world_instability(b/a, 1/a)
+    draw_diagram(a_space, b_space, instability)
+
+
+if __name__ == "__main__":
+    k_d_diagram(16)
--- two_group_upperbound.py
+++ two_group_upperbound.py
@@ -1,16 +1,14 @@
 from numpy import linspace
+from time import time
+from utils import relative_cost_reduction, absolute_cost_reduction
 
-ITER = 101
-
-def relative_cost_reduction(old_costs, new_costs):
-    return old_costs - new_costs
-    return 1 - new_costs / old_costs
+ITERATIONS = 101
 
 
 def world_instability(k, d):
     min_eps_partition = 1.
     a_res = ()
-    for a in linspace(0., 1., ITER):
+    for a in linspace(0., 1., ITERATIONS):
         groups = [(1., a, 0.)]
         if k > a:
             groups.append((0., k-a, d))
@@ -21,7 +19,7 @@ def world_instability(k, d):
                 a_res = (a, "F")
             else:
                 a_res = (a, int(a * 100))
-    for a in linspace(0., 1., ITER):
+    for a in linspace(0., 1., ITERATIONS):
         groups = [(a, k, d)]
         if a < 1.:
             groups.append((1.-a, 0., 0.))
@@ -32,7 +30,7 @@ def world_instability(k, d):
                 a_res = (a, "R")
             else:
                 a_res = (a, "U")
-    for b in linspace(0., min(d, 1.), ITER):
+    for b in linspace(0., min(d, 1.), ITERATIONS):
         groups = [(1., 1., b)]
         if k > 1:
             groups.append((0., k-1., d))
@@ -47,8 +45,6 @@ def groups_instability(groups, d):
     left_groups = list()
     right_groups = list()
     for left, right, median in groups:
-        # if (left + right) < 0.01:
-        #     print("WARNING")
         left_costs = 1. / (left + right) + median
         right_costs = 1. / (left + right) + d - median
         if left:
@@ -64,7 +60,7 @@ def groups_instability(groups, d):
 
 def optimize_costs(size, left_costs, right_costs, d):
     transport_costs = 1. / size
-    # (transport_costs + dist) / left_costs = (transport_costs + d - dist) / right_costs
+    # equation for a new median: (transport_costs + dist) / left_costs = (transport_costs + d - dist) / right_costs
     new_median = ((transport_costs + d) * left_costs - transport_costs * right_costs) / (left_costs + right_costs)
     if new_median < 0.:
         new_median = 0.
@@ -72,7 +68,7 @@ def optimize_costs(size, left_costs, right_costs, d):
         new_median = d
     new_left_costs = transport_costs + new_median
     new_right_costs = transport_costs + d - new_median
-    return min(relative_cost_reduction(left_costs, new_left_costs), relative_cost_reduction(right_costs, new_right_costs))
+    return min(cost_reduction(left_costs, new_left_costs), cost_reduction(right_costs, new_right_costs))
 
 
 def max_undef_group_desire(left_groups, right_groups, d):
@@ -109,27 +105,27 @@ def max_side_group_desire(left_groups, right_groups, d, reversed=False):
     desires = [0.]
     # need only cases not evaluated in mid_separation
     if len(left_groups) >= 1:
-        desires.append(relative_cost_reduction(left_groups[0][1], 1./left_groups[0][0]))
+        desires.append(cost_reduction(left_groups[0][1], 1./left_groups[0][0]))
 
     if len(left_groups) >= 2:
-        desires.append(relative_cost_reduction(left_groups[1][1], 1. / left_groups[1][0]))
+        desires.append(cost_reduction(left_groups[1][1], 1. / left_groups[1][0]))
 
         old_costs = min(left_groups[0][1], left_groups[1][1])
-        desires.append(relative_cost_reduction(old_costs, 1. / (left_groups[0][0] + left_groups[1][0])))
+        desires.append(cost_reduction(old_costs, 1. / (left_groups[0][0] + left_groups[1][0])))
 
     if not reversed and right_groups:
         if len(right_groups) == 2:
             if right_groups[0][0] < 1.:
                 new_size = left_groups[0][0] + right_groups[0][0]
                 desires.append(min(
-                    relative_cost_reduction(left_groups[0][1], 1. / new_size),
-                    relative_cost_reduction(right_groups[0][1], 1. / new_size + d)
+                    cost_reduction(left_groups[0][1], 1. / new_size),
+                    cost_reduction(right_groups[0][1], 1. / new_size + d)
                 ))
             if right_groups[1][0] < 1.:
                 new_size = left_groups[0][0] + right_groups[1][0]
                 desires.append(min(
-                    relative_cost_reduction(left_groups[0][1], 1. / new_size),
-                    relative_cost_reduction(right_groups[1][1], 1. / new_size + d)
+                    cost_reduction(left_groups[0][1], 1. / new_size),
+                    cost_reduction(right_groups[1][1], 1. / new_size + d)
                 ))
 
     if reversed and right_groups:
@@ -137,127 +133,66 @@ def max_side_group_desire(left_groups, right_groups, d, reversed=False):
             if left_groups[0][0] > 1.:
                 new_size = left_groups[0][0] + right_groups[0][0]
                 desires.append(min(
-                    relative_cost_reduction(left_groups[0][1], 1. / new_size),
-                    relative_cost_reduction(right_groups[0][1], 1. / new_size + d)
+                    cost_reduction(left_groups[0][1], 1. / new_size),
+                    cost_reduction(right_groups[0][1], 1. / new_size + d)
                 ))
             if left_groups[1][0] > 1.:
                 new_size = left_groups[1][0] + right_groups[0][0]
                 desires.append(min(
-                    relative_cost_reduction(left_groups[1][1], 1. / new_size),
-                    relative_cost_reduction(right_groups[0][1], 1. / new_size + d)
+                    cost_reduction(left_groups[1][1], 1. / new_size),
+                    cost_reduction(right_groups[0][1], 1. / new_size + d)
                 ))
 
         if len(left_groups) == 1:
             if len(right_groups) >= 1:
                 new_size = left_groups[0][0] + right_groups[0][0]
                 desires.append(min(
-                    relative_cost_reduction(left_groups[0][1], 1. / new_size),
-                    relative_cost_reduction(right_groups[0][1], 1. / new_size + d)
+                    cost_reduction(left_groups[0][1], 1. / new_size),
+                    cost_reduction(right_groups[0][1], 1. / new_size + d)
                 ))
             if len(right_groups) == 2:
                 new_size = left_groups[0][0] + right_groups[1][0]
                 desires.append(min(
-                    relative_cost_reduction(left_groups[0][1], 1. / new_size),
-                    relative_cost_reduction(right_groups[1][1], 1. / new_size + d)
+                    cost_reduction(left_groups[0][1], 1. / new_size),
+                    cost_reduction(right_groups[1][1], 1. / new_size + d)
                 ))
 
                 new_size = left_groups[0][0] + right_groups[0][0] + right_groups[1][0]
                 desires.append(min(
-                    relative_cost_reduction(left_groups[0][1], 1. / new_size),
-                    relative_cost_reduction(right_groups[0][1], 1. / new_size + d),
-                    relative_cost_reduction(right_groups[1][1], 1. / new_size + d)
+                    cost_reduction(left_groups[0][1], 1. / new_size),
+                    cost_reduction(right_groups[0][1], 1. / new_size + d),
+                    cost_reduction(right_groups[1][1], 1. / new_size + d)
                 ))
 
     return max(desires)
 
 
-from time import time
-import numpy as np
-import scipy.misc as smp
-
-def diagram():
-    size = 4
-    data = np.zeros((size, size, 3), dtype=np.uint8)
-    # data[512, 512] = [254, 0, 0]  # Makes the middle pixel red
-    # data[512, 513] = [0, 0, 255]  # Makes the next pixel blue
-    for k, kp in zip(linspace(1, 2, size), range(size)):
-        for d, dp in zip(linspace(0.5, 1, size), range(size)):
-            instab, a = world_instability(k, d)
-            if instab < 0.0000001:
-                a = (0, "S")
-            # if k == 1.30 and d == 0.64:
-            #     print("_", end="")
-            if a[1] == "S":
-                data[kp, dp] = [255, 255, 255]
-            elif a[1] == "U":
-                data[kp, dp] = [255, 0, 0]
-            elif a[1] == "M":
-                data[kp, dp] = [0, 255, 0]
-            elif a[1] == "F":
-                data[kp, dp] = [0, 0, 255]
-            else:
-                data[kp, dp] = [128 + int(a[1]) * 10]
-
-    img = smp.toimage(data)
-    img.show()
-
-
-def diagram2():
-    size = 64
-    data = np.zeros((size, size, 3), dtype=np.uint8)
-    # data[512, 512] = [254, 0, 0]  # Makes the middle pixel red
-    # data[512, 513] = [0, 0, 255]  # Makes the next pixel blue
-    for k, kp in zip(linspace(1, 2, size), range(size)):
-        for d, dp in zip(linspace(1.6, 3.2, size), range(size)):
-            instab, a = world_instability(d/k, 1/k)
-            if instab < 0.0000001:
-                a = (0, "S")
-            # if k == 1.30 and d == 0.64:
-            #     print("_", end="")
-            if a[1] == "S":
-                data[kp, dp] = [255, 255, 255]
-            elif a[1] == "U":
-                data[kp, dp] = [255, 0, 0]
-            elif a[1] == "M":
-                data[kp, dp] = [0, 255, 0]
-            elif a[1] == "F":
-                data[kp, dp] = [0, 0, 255]
-            else:
-                data[kp, dp] = [128 + int(a[1]) * 10]
-
-    img = smp.toimage(data)
-    img.show()
-
-
-def main():
-    # print(world_instability(73./56., 0.6363245))
+def upperbound(d_space, k_start=1., k_end=1.62, k_iter=1000):
     t = time()
-    for k in linspace(1.30, 1.31, 41):
-        res = 0.
+    for k in linspace(k_start, k_end, k_iter):
+        eps = 0.
         d_res = 0
         a_res = 0
-        for d in linspace(k/(1 + k), 1/k, 300):
+        for d in d_space(k):
             next_instab, a = world_instability(k, d)
-            if next_instab > res:
-                res, d_res, a_res = next_instab, d, a
+            if next_instab > eps:
+                eps, d_res, a_res = next_instab, d, a
 
-        # print("TIME: " + str(time() - t))
-        print(k, d_res, a_res, res)
-
-
-def main_abs():
-    t = time()
-    for k in linspace(1.295, 1.325, 10):
-        res = 0.
-        d_res = 0
-        a_res = 0
-        for d in linspace(0, 1, 2000):
-            next_instab, a = world_instability(k, d)
-            if next_instab > res:
-                res, d_res, a_res = next_instab, d, a
-
-        # print("TIME: " + str(time() - t))
-        print(k, d_res, a_res, res)
+        print("Time passed:", str(time() - t))
+        print(f"k={round(k, 2)}, d={round(d_res, 2)}, a_res={a_res}, eps={eps}")
 
 
-main_abs()
+def upperbound_rel(k_start=1., k_end=1.62, k_iter=1000, d_iter=300):
+    d_space = lambda k: linspace(k / (1 + k), 1 / k, d_iter)
+    upperbound(d_space, k_start, k_end, k_iter)
+
+
+def upperbound_abs(k_start=1., k_end=2., k_iter=1000, d_iter=300):
+    d_space = linspace(0, 1, d_iter)
+    upperbound(d_space, k_start, k_end, k_iter)
+
+
+if __name__ == "__main__":
+    ITERATIONS = 101
+    cost_reduction = relative_cost_reduction
+    upperbound_rel(1.30, 1.31, 41, 300)
